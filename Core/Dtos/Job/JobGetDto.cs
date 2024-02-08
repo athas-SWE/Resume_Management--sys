2ddc5e738849626f9360704c65e5f14312c06ce7
Athas @ara <83549525+it20301422@users.noreply.github.com>
2024-02-08 16:14:11 +0530

    first commit

--- Core/Dtos/Job/JobGetDto.cs
+++ Core/Dtos/Job/JobGetDto.cs
@@ -1,6 +1,15 @@
-﻿namespace Backend_Management.Core.Dtos.Job
+﻿
+using Backend_Management.Core.Enums;
+
+namespace backend.Core.Dtos.Job
 {
     public class JobGetDto
     {
+        public long ID { get; set; }
+        public string Title { get; set; }
+        public JobLevel Level { get; set; }
+        public long CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
