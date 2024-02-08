b3d6021d9b122a9b21b81acb85f8f0c3ff5eae56
Athas @ara <83549525+it20301422@users.noreply.github.com>
2024-02-08 16:18:18 +0530

    first commit

--- Core/Dtos/Candidate/CandidateCreateDto.cs
+++ Core/Dtos/Candidate/CandidateCreateDto.cs
@@ -1,6 +1,12 @@
-﻿namespace Backend_Management.Core.Dtos.Candidate
+﻿namespace backend.Core.Dtos.Candidate
 {
     public class CandidateCreateDto
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string CoverLetter { get; set; }
+        public long JobId { get; set; }
     }
 }
